e637a5d46db7bf8c69ca108ef4a9432e62e78bcc
RainerMorales <rainermorales213@gmail.com>
2025-06-06 23:17:28 +0800

    add cert

--- src/app/Aboutme/page.tsx
+++ src/app/Aboutme/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { CgWebsite } from "react-icons/cg";
 import Image from "next/image";
+import { PiCertificateFill } from "react-icons/pi";
+import { BlurFade } from "@/components/magicui/blur-fade";
 function Aboutme() {
   return (
     <>
@@ -19,46 +21,48 @@ function Aboutme() {
             About Me
           </div>
         </div>
-        <div className="p-6 ">
-          <div className=" col-span-3 opacity-80  border rounded-md shadow-md p-6">
-            Hi! 👋 I'm Rainer Morales, a front-end developer focused on building
-            interactive and responsive web apps using React (or Next.js) and
-            Firebase 🔥. I enjoy crafting smooth, clean interfaces with Tailwind
-            CSS and Framer Motion — bringing ideas to life through animation and
-            modern design. I graduated with a Bachelor of Science in Information
-            Technology from Eastwoods Professional College of Science and
-            Technology in July 2024 🎓. I love playing the organ and piano 🎹 —
-            music helps me stay creative and focused.
-          </div>
-          <div className="grid grid-cols-4">
-            <div className="col-span-2 opacity-80  border rounded-md shadow-md p-6">
-              <Image
-                className=""
-                src="/intern.jpg"
-                width={300}
-                height={80}
-                alt="intern"
-              ></Image>
+        <div className="grid grid-cols-4 gap-4 p-6">
+          <BlurFade className=" col-span-4 border rounded-md shadow-md p-6">
+            <div className=" opacity-80 ">
+              Hi! 👋 Im Rainer Morales, front-end developer focused on building
+              interactive and responsive web apps using React (or Next.js) and
+              Firebase 🔥. I enjoy crafting smooth, clean interfaces with
+              Tailwind CSS and Framer Motion — bringing ideas to life through
+              animation and modern design. I graduated with a Bachelor of
+              Science in Information Technology from Eastwoods Professional
+              College of Science and Technology in July 2024 🎓. I love playing
+              the organ and piano 🎹 — music helps me stay creative and focused.
             </div>
-            <div className="col-span-2 opacity-80  border rounded-md shadow-md p-6">
-              <Image
-                className=""
-                src="/east.jpg"
-                width={300}
-                height={80}
-                alt="intern"
-              ></Image>
+          </BlurFade>
+          <BlurFade
+            inView
+            delay={0.15 * 2}
+            className="border rounded-md shadow-md p-6 col-span-4"
+          >
+            <div className="flex items-center gap-2 p-4 font-bold text-xl">
+              <PiCertificateFill />
+              My Certificates
             </div>
-            <div className="col-span-4 opacity-80  border rounded-md shadow-md p-6">
-              <Image
-                className=""
-                src="/ircite.jpg"
-                width={300}
-                height={80}
-                alt="intern"
-              ></Image>
+            <div className="flex flex-col lg:flex-row md:flex-row  items-center justify-center gap-4 ">
+              {[
+                { src: "/intern.jpg", alt: "intern logo" },
+                { src: "/east.jpg", alt: "east logo" },
+                { src: "/ircite.jpg", alt: "ircite logo" },
+              ].map((img, i) => (
+                <div
+                  key={i}
+                  className="border rounded-md shadow-md w-72 h-60 relative overflow-hidden"
+                >
+                  <Image
+                    src={img.src}
+                    alt={img.alt}
+                    fill
+                    className="object-cover"
+                  ></Image>
+                </div>
+              ))}
             </div>
-          </div>
+          </BlurFade>
         </div>
       </div>
     </>
